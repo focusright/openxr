4f28526b1409f847814150ec4b6840836e240e8c
Mike Lee <rockespecialee@gmail.com>
2021-09-09 14:52:01 -0700

    add shutdown thread

--- bare-bones/openxr-opengl-barebone/openxr-opengl-barebone/main.cpp
+++ bare-bones/openxr-opengl-barebone/openxr-opengl-barebone/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <thread>
 
 int main(int argc, char* argv[]) {
     static bool quitKeyPressed = false;
+    auto exitPollingThread = std::thread{[] {
+        std::cout << "Press any key to shutdown...";
+        (void)getchar();
+        quitKeyPressed = true;
+    }};
+    exitPollingThread.detach();
+
     do {
 
     } while (!quitKeyPressed);
