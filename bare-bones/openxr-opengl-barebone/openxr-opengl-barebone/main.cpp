efaed5c153e9ff9ae98cb12446a21ee13fc5f84c
Mike Lee <rockespecialee@hotmail.com>
2021-09-13 03:14:50 -0700

    icosahedron sphere implemented

--- bare-bones/openxr-opengl-barebone/openxr-opengl-barebone/main.cpp
+++ bare-bones/openxr-opengl-barebone/openxr-opengl-barebone/main.cpp
@@ -1705,8 +1705,26 @@ namespace Geometry {
     constexpr Vertex c_planeVertices[] = { CUBE_SIDE(LTB, RTB, RTF, LTB, RTF, LTF, DarkGreen) };
     constexpr unsigned short c_planeIndices[] = { 0,  1,  2,  3,  4,  5 };
 
-    constexpr Vertex c_sphereVertices[] = { TRIANGLE_SIDE(LBF, LTF, RTF, MidRed) };
-    constexpr unsigned short c_sphereIndices[] = { 0,  1,  2 };
+    //constexpr Vertex c_sphereVertices[] = { TRIANGLE_SIDE(LBF, LTF, RTF, MidRed) };
+    //constexpr unsigned short c_sphereIndices[] = { 0,  1,  2 };
+
+    float X=.525731112119133606f;
+    float Z=.850650808352039932f;
+    float N=0.f;
+    XrVector3f color = MidRed;
+
+    const Vertex c_sphereVertices[] = {
+        {{-X,N,Z},color}, {{X,N,Z},color}, {{-X,N,-Z},color}, {{X,N,-Z},color},
+        {{N,Z,X},color}, {{N,Z,-X},color}, {{N,-Z,X},color}, {{N,-Z,-X},color},
+        {{Z,X,N},color}, {{-Z,X, N},color}, {{Z,-X,N},color}, {{-Z,-X, N},color}
+    };
+ 
+    constexpr unsigned short c_sphereIndices[] = {
+        0,4,1,0,9,4,9,5,4,4,5,8,4,8,1,
+        8,10,1,8,3,10,5,3,8,5,2,3,2,7,3,
+        7,10,3,7,6,10,7,11,6,11,0,6,0,1,6,
+        6,1,10,9,0,11,9,11,2,9,2,5,7,2,11
+    };
 }
 
 void CheckShader(GLuint shader) {
